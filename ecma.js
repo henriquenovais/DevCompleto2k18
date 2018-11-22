3fa450e8731a29709329cc93de18cd5072bab123
Henrique Novais <henriquennoliveira@gmail.com>
2018-11-22 20:47:59 -0300

    Add ecmascript basic story notes

---
+++ ecma.js
@@ -0,0 +1,7 @@
+///ECMAscript is a language which has the purpose
+//of being the stand pattern of script languages such as javascript
+
+/*
+In order to compile ECMAscript code it is required the use
+of a JavaScript library. The main library for compiling ECMAscript is Babel.js .
+*/ 
